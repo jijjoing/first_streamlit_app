facfd4222ab25fae61839ca5ac3e985b129325f8
jijjoing <95073577+jijjoing@users.noreply.github.com>
2023-05-24 17:10:06 +0900

    Update streamlit_app.py
    주석 달기

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") -- S3 데이터 가지고 오기
+my_fruit_list = my_fruit_list.set_index('Fruit') -- 과일 이름으로 선택할 수 있도록 index 설정
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) -- 설정된 인덱스를 선택 가능하도록 설정
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list) -- 저장된 S3 데이터를 데이터프레임으로 설정
