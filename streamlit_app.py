a3a98b7c06a41ac4a88869aa8d08ddf57c9102d2
jijjoing <95073577+jijjoing@users.noreply.github.com>
2023-05-26 16:28:35 +0900

    Update streamlit_app.py
    Edit Pick some fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit') # 과일 이름으로 선택할 수 있도록 index 설정
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) # 설정된 인덱스를 선택 가능하도록 설정
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) # 설정된 인덱스를 선택 가능하도록 설정
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries']) # 픽스 인덱스 값 설정
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list) # 저장된 S3 데이터를 데이터프레임으로 설정
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+
