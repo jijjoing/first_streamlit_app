e889b2ca6e3e2c40e70993801b68f6351915882f
jijjoing <95073577+jijjoing@users.noreply.github.com>
2023-05-26 16:39:17 +0900

    Update streamlit_app.py
    픽스 데이터가 저장된 버전으로 저장

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,15 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") # S3 데이터 가지고 오기
 my_fruit_list = my_fruit_list.set_index('Fruit') # 과일 이름으로 선택할 수 있도록 index 설정
 
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) # 설정된 인덱스를 선택 가능하도록 설정
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries']) # 픽스 인덱스 값 설정
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries']) # 픽스 인덱스 값 설정
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list) # 저장된 S3 데이터를 데이터프레임으로 설정
+# streamlit.dataframe(my_fruit_list) # 저장된 S3 데이터를 데이터프레임으로 설정
+streamlit.dataframe(fruits_selected) # 픽스 데이터가 설정된 버전으로 변경
 
 
