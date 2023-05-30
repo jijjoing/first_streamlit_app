a8c94ee5842b120d49d6fcadf98764b6be02a27d
jijjoing <95073577+jijjoing@users.noreply.github.com>
2023-05-30 23:39:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,28 +11,25 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") # S3 데이터 가지고 오기
-my_fruit_list = my_fruit_list.set_index('Fruit') # 과일 이름으로 선택할 수 있도록 index 설정
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# streamlit.dataframe(my_fruit_list) # 저장된 S3 데이터를 데이터프레임으로 설정
-streamlit.dataframe(fruits_to_show) # 픽스 데이터가 설정된 버전으로 변경
+# streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show) 
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') # 텍스트 입력 상자
-streamlit.write('The user entered ', fruit_choice) # API 일부 호출
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') 
+streamlit.write('The user entered ', fruit_choice) 
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# [fruityvice_normalized]변수에 정규화된 json 파일을 저장
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# 저장한 변수를 데이터프레임으로 변경
 streamlit.dataframe(fruityvice_normalized)
 
-
-# snowflake 파이썬 커넥터 연결
 # import snowflake.connector
