a066b7e2a472fb818cad365cb575282e7fac3afb
jijjoing <95073577+jijjoing@users.noreply.github.com>
2023-05-31 00:22:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,6 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit') # 텍스트 입력 상자
 streamlit.text("Thanks for adding jackfruit")
 
+streamlit.write('Thanks for adding ', add_my_fruit)
+# This will not correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
