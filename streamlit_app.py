82e07eaf1e0d992e7e2d9d90ae374b51a6b908f6
jijjoing <95073577+jijjoing@users.noreply.github.com>
2023-05-31 12:45:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,33 +40,29 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
       
-except URLErrir as e:
+except URLError as e:  # 에러 발생 시 스탑
     # don't run anything past here while we troubleshoot
     streamlit.stop()
-
-
 
 streamlit.header("The fruit load list contains:")
 # Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
+         my_cur.execute("select * from fruit_load_list")  # DB 데이터 가져오기
+         return my_cur.fetchall()  # 전체 결과 불러오기
 # Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])  # DB 연결
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
-
-
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
          my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
          return "Thanks for adding " + new_fruit
-        
+ 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?') # 텍스트 입력 상자
 
 if streamlit.button('Get Fruit List'):
@@ -75,9 +71,5 @@ if streamlit.button('Get Fruit List'):
    my_data_rows = get_fruit_load_list()
    my_cnx.close()
    streamlit.dataframe(my_data_rows)
-#    back_from_function = insert_row_snowflake(add_my_fruit)
-#    streamlit.text(back_from_function)
-    
-# # This will not correctly, but just go with it for now
-# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-streamlit.write('Thanks for adding ', add_my_fruit)
+ 
+# streamlit.write('Thanks for adding ', add_my_fruit)
