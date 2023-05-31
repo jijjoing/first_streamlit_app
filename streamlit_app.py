41d879e6bb25e235e2d8ae12e57405f938faa3a2
jijjoing <95073577+jijjoing@users.noreply.github.com>
2023-05-31 09:10:50 +0900

    Update streamlit_app.py
    if문

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try :
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
-   else:
+    else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         # [fruityvice_normalized]변수에 정규화된 json 파일을 저장
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
